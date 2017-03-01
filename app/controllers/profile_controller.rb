5107a6a74e14369b848a5c0b83e74127eb00a48a
prodhan29 <02nahid02@gmail.com>
2017-03-01 18:02:33 +0600

    carrierwave added

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,31 @@
+class ProfileController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    profile = Profile.new(profile_params)
+    profile.save!
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def profile_attachment
+    @profile = Profile.find_by_name(params[:profile_name])
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :attachment)
+  end
+
+end
---
+++ test/controllers/face_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FaceControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
