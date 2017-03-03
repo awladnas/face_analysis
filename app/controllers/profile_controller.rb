13cf4bf1c8c73da208238c3f13ceba12daf7a035
Biozid Bostami <biozid@bdipo.com>
2017-03-03 12:08:17 +0600

    -print response to the view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,7 +1,12 @@
-
 class ProfileController < ApplicationController
+  include HTTParty
   def index
-
+    @face_info =  HTTParty.post("https://api-us.faceplusplus.com/facepp/v3/detect", :body=> {
+        :api_key => "0BPOcncjMOdVxsYce0K8eAAEDBvlT6_h",
+        :api_secret => "r8L47AnUGN34VJL-5ftp6rHZ9UHDKcSL",
+        :image_url => "https://us-mc-test-asset.oss-us-west-1.aliyuncs.com/mc-official/images/demo/demo-pic6.jpg"
+    } )
+   render plain: "#{@face_info}"
   end
 
   def new
@@ -22,6 +27,38 @@ class ProfileController < ApplicationController
 
   def profile_attachment
     @profile = Profile.find_by_name(params[:profile_name])
+  end
+
+  def detect_face
+    res=  HTTParty.post("https://api-us.faceplusplus.com/facepp/v3/detect", :body=> {
+        :api_key => "0BPOcncjMOdVxsYce0K8eAAEDBvlT6_h",
+        :api_secret => "r8L47AnUGN34VJL-5ftp6rHZ9UHDKcSL",
+        :image_url => "https://us-mc-test-asset.oss-us-west-1.aliyuncs.com/mc-official/images/demo/demo-pic6.jpg"
+    } )
+
+    puts res.inspect
+  end
+
+  def serach_face
+    @face_info =  HTTParty.post("https://api-us.faceplusplus.com/facepp/v3/search", :body=> {
+        :api_key => "0BPOcncjMOdVxsYce0K8eAAEDBvlT6_h",
+        :api_secret => "r8L47AnUGN34VJL-5ftp6rHZ9UHDKcSL",
+        :face_token1 => "f3cb5356007a60eb8517dd961a5b5b0f",
+        :outer_id => "1488448621,ca5eecd4-8e59-484e-9d50-4a72ce6be96a"
+    } )
+
+    puts res.inspect
+  end
+
+  def compare_face
+    @face_info =  HTTParty.post("https://api-us.faceplusplus.com/facepp/v3/search", :body=> {
+        :api_key => "0BPOcncjMOdVxsYce0K8eAAEDBvlT6_h",
+        :api_secret => "r8L47AnUGN34VJL-5ftp6rHZ9UHDKcSL",
+        :face_token1 => "f3cb5356007a60eb8517dd961a5b5b0f",
+        :face_token2 => "f3cb5356007a60eb8517dd961a5b5b0f"
+    } )
+
+    puts res.inspect
   end
 
   private
